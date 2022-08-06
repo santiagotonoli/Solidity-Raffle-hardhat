d138bd60f437169cdec4bb046c3581f2245b976d
stonoli <stonolidm@gmail.com>
2022-08-06 02:28:20 +0200

    deploy mock and deploy raffle js file

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,24 @@
+const {developmentChains} = require("../helper-hardhat-config");
+const { getNamedAccounts, deployments, network, ethers } = require("hardhat")
+const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is the premium. it cost that to use oracle
+const GAS_PRICE_LINK = 1e9 //link per gas. calculated value base on the gas price of the chain
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const chainId = network.config.chainId
+    const args = [BASE_FEE, GAS_PRICE_LINK]
+
+    if(developmentChains.includes(network.name)){
+        log("Local network detected! Deploying mocks...")
+        // deploy a mock vrfcoordinator
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            args: args,
+            log: true
+            });
+            log("Mocks deployed!")
+            log("-----------------------------------------------------")
+            
+    }
+}
---
+++ helper-hardhat-config.js
@@ -0,0 +1,29 @@
+const networkConfig = {
+    4: {
+        name: "rinkeby",
+        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        subscriptionId: "0",
+        callbackGasLimit: "500000",
+        interval: "30"
+    },
+    31337: {
+        name: "hardhat",
+        entranceFee: ethers.utils.parseEther("0.01"),
+        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+        callbackGasLimit: "500000",
+        interval: "30"
+    }
+}
+
+const developmentChains = ["hardhat", "localhost"];
+const DECIMALS = 8;
+const INITIAL_ANSWER = 200000000000;
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    DECIMALS,
+    INITIAL_ANSWER
+}
